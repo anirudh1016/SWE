381997a5e00eef0e93cf7ea911eef3df9b26ca03
Rudy <36571207+anirudh1016@users.noreply.github.com>
2019-02-26 10:32:08 -0500

    Add files via upload

---
+++ quizScheduleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class quizScheduleTest extends TestCase 
+{
+	@Test
+	public void firstTest()
+	{
+		quizzes QuizList = new quizzes();
+		
+	}
+}
